20b66abf9eb078b5cdff9016078eb293455cde75
FirasSat <firas.sy.sa@gmail.com>
2023-01-06 19:19:23 +0300

    فراس تعديل الجمعة قبل يومين من التسليم :)

---
+++ lang/ar/message.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | message Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    've' => 'خطأ في التحقق',
+    'NA' => 'غير مسموح لك',
+    'timeS' => 'تم حجز الوقت بنجاح',
+    'timeF' => 'الوقت غير متوفر او محجوز',
+    'timeNF' => 'الوقت غير موجود',
+    'timeDS' => 'تم حذف الوقت بنجاح',
+    'timeisAdd' => 'تم اضافة الوقت',
+];
---
+++ lang/en/message.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | message Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    've' => 'Validation error',
+    'NA' => 'You Are Not Allowed',
+    'timeS' => 'Time Created Successfully',
+    'timeF' => 'The time is not available or reserved',
+    'timeNF' => 'Time Not Found',
+    'timeDS' => 'Time Delete Successfully',
+    'timeisAdd' => 'Time Is Added',
+    'userS' => 'User Created Successfully',
+
+];
